27a14e6c75d68c26e0c6f9916d6d61061d81c8cd
shavalibadvel <55553298+shavalibadvel@users.noreply.github.com>
2026-05-20 18:19:06 +0530

    final changes

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./authSlice";
 import productsReducer from "./productsSlice";
+//godolfoj
 export const store = configureStore({
   reducer: {
     auth: authReducer,
